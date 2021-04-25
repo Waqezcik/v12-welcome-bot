6b2f8ea56b920adc76bb9daba6882aeaba70167c
Waqez <83038430+Waqezcik@users.noreply.github.com>
2021-04-25 22:55:02 +0400

    README.MD

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# v12-welcome-bot
+
+A Advanced Welcome Bot With Image Support (You Can Change Both: Welcome & Bye)
+
+- Image (Join, Leave)
+- Changeable Prefix
+- Etc
+
+### Setup
+
+- Download / Export This Repository & Make Sure To Install [Node.js](https://nodejs.org/en/) & [Visual Studio Code (Only If You Don't Have Any Code Editor)](https://code.visualstudio.com/)
+- Open Project In Visual Studio Code (Editor) & (Visual Studio Code) To Open Console / Terminal - **Ctrl + A**
+- In Your Project Console / Terminal Type This - **npm i**
+- And Wait For Them (Packages) To Install...
+- After Installing Done, Go To Config.js And
+- Fill Information & After That Type This In Your Console / Terminal - **_node ._** Or **_node server.js_**
+- When Bot Is Ready, You Will See A Message In Console / Terminal
+- Enjoy <3
+
+
+ **And You Know You Can Add Your Audio Files Into The Project It Will Not Add It For Now.**
+
+### Credits
+
+- Written by Waqez 
+
+### Other
+
+Note: Please Make Sure You Know Basics Or I Will Report You :)
+
+- Author - Waqez
+- License - [Mit]
---
+++ ses.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const tokens = [
+    "ODM0Nzk0MTE1NjgxMDkxNjA0.YIGEmg.OL-AAUBlOTFm38bKigSdUHuTB5w",
+    "ODM0Nzk0MjQ1MTUxMzkxNzY0.YIGEuQ.d4ItgvbnR9PrD3FbJpwKrRWGx50",
+    "ODM0Nzk0MzgwNDE3OTU3ODg4.YIGE2g.rzBFQYHaE_HyimSU0rs3syMIekY"
+];
+const chnls = [
+    "834793765097046036",
+    "834793801208823829",
+    "834793828489363476" 
+];
+const selamlı = [];
+for (let index = 0; index < tokens.length; index++) {
+    const token = tokens[index];
+    const client = new Discord.Client();
+    client.login(token);
+    let concon;
+    client.on('ready', async () => {
+        console.log(client.user.username);
+        await client.user.setActivity({
+            name: "Waqez's Welcome Bots",
+            type: "LISTENING"
+        });
+        concon = await client.channels.cache.get(chnls[index]).join()
+    });
+    let ses;
+    client.on('voiceStateUpdate', async (prev, cur) => {
+        if (cur.member.user.bot) return;
+        if (cur.channel && (cur.channel.id === chnls[index])) {
+            if (cur.channelID === prev.channelID) return;
+            if (selamlı.includes(cur.member.id) && (cur.member.roles.highest.rawPosition < cur.guild.roles.cache.get("834794598621904896").rawPosition)) {//ROLID
+                //console.log(selamlı);
+                ses = await concon.play('./tekrardan.mp3');
+                return;
+            }
+            if ((cur.member.roles.highest.rawPosition < cur.guild.roles.cache.get("834794598621904896").rawPosition)) { //ROLID
+                ses = await concon.play('./hosgeldin.mp3');
+                selamlı.push(cur.member.user.id);
+            } else if (cur.member.roles.highest.rawPosition > cur.guild.roles.cache.get('834794598000754749').rawPosition) {//ROLID
+                ses = await concon.play('./yetkili.mp3');
+                selamlı.push(cur.member.user.id);
+            }
+        }
+        if (prev.channel && (prev.channel.id === chnls[index]) && (prev.channel.members.size === 1) && ses) ses.end();
+    });
+    client.on('guildMemberUpdate', async (prev, cur) => {
+        if (concon.channel.members.some(biri => biri.user.id === cur.user.id)) {
+            if ((prev.roles.highest.rawPosition < cur.roles.highest.rawPosition)) {
+                ses = await concon.play('./elveda.mp3');
+            }
+        } else return;
+    });
+    client.on('voiceStateUpdate', async (prev, cur) => {
+        if (cur.member.id === client.user.id) concon = await client.channels.cache.get(chnls[index]).join();
+    })
+}
+
